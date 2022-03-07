6a44869aa846245eb0f8bb4090d2848bf888aeb9
yuzihan <yuzihanyuzihan@163.com>
2022-03-07 21:29:35 +0800

    feat: 206_递归法反转链表

---
+++ py/linked_list/206_reverseList_递归法.py
@@ -0,0 +1,34 @@
+'''
+Author: your name
+Date: 2022-03-07 21:28:54
+LastEditTime: 2022-03-07 21:29:04
+LastEditors: your name
+Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+FilePath: /leetcode_bake/py/linked_list/206_reverseList_递归法.py
+'''
+"""
+206. 反转链表
+给你单链表的头节点 head ，请你反转链表，并返回反转后的链表
+"""
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        # 思路：采用递归方法，确定递归终止条件，划分为子问题，递归分为递和归两个步骤；
+        # 实际归的过程相当于逆序不断在尾部接上前一个节点
+        #  1. 递归终止条件, 其实就是递的终止条件，终止完后不断往前回归
+        if not head or not head.next:
+            return head
+
+        # 2. 递归调用
+        p = self.reverseList(head.next)  # p相当于子问题每次排好序后返回的头节点
+
+        # 3. 子问题处理
+        head.next.next = head
+        head.next = None  # 易错点
+
+        # 4. 返回结果
+        return p
